6eba7c3c6718134800c0e7700f38eb39d216c9ad
Baptiste Taillard <baptiste.taillard@es.nestle.com>
2025-11-06 06:53:28 +0100

    last one for real

--- udm-explorer/src/components/ParserGenerator.js
+++ udm-explorer/src/components/ParserGenerator.js
@@ -1,5 +1,5 @@
 // src/components/ParserGenerator.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 // Import UDM schemas to check field properties
